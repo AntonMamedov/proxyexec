fb5a56da505fb9221f30fda7175be0bf6f551283
Anton Mamedov <a.mamedov@corp.mail.ru>
2022-05-18 20:23:44 +0300

    added find sys_call_table addr

--- proxyexec.c
+++ proxyexec.c
@@ -5,6 +5,69 @@
 #include <linux/socket.h>
 #include <linux/kallsyms.h>
 
+typedef unsigned long (*kln_p)(const char*);
+
+#include <linux/kprobes.h>
+
+#define KPROBE_PRE_HANDLER(fname) static int __kprobes fname(struct kprobe *p, struct pt_regs *regs)
+
+long unsigned int kln_addr = 0;
+unsigned long (*kln_pointer)(const char* name) = NULL;
+
+static struct kprobe kp0, kp1;
+
+KPROBE_PRE_HANDLER(handler_pre0) {
+    kln_addr = (--regs->ip);
+
+    return 0;
+}
+
+KPROBE_PRE_HANDLER(handler_pre1) {
+    return 0;
+}
+
+static int do_register_kprobe(struct kprobe* kp, char* symbol_name, void* handler) {
+    int ret;
+
+    kp->symbol_name = symbol_name;
+    kp->pre_handler = handler;
+
+    ret = register_kprobe(kp);
+    if (ret < 0) {
+        pr_err("do_register_kprobe: failed to register for symbol %s, returning %d\n", symbol_name, ret);
+        return ret;
+    }
+
+    pr_info("Planted krpobe for symbol %s at %p\n", symbol_name, kp->addr);
+
+    return ret;
+}
+
+// this is the function that I have modified, as the name suggests it returns a pointer to the extracted kallsyms_lookup_name function
+kln_p get_kln_p(void) {
+    int status;
+
+    status = do_register_kprobe(&kp0, "kallsyms_lookup_name", handler_pre0);
+
+    if (status < 0) return NULL;
+
+    status = do_register_kprobe(&kp1, "kallsyms_lookup_name", handler_pre1);
+
+    if (status < 0) {
+        // cleaning initial krpobe
+        unregister_kprobe(&kp0);
+        return NULL;
+    }
+
+    unregister_kprobe(&kp0);
+    unregister_kprobe(&kp1);
+
+    pr_info("kallsyms_lookup_name address = 0x%lx\n", kln_addr);
+
+    kln_pointer = (unsigned long (*)(const char* name)) kln_addr;
+
+    return kln_pointer;
+}
 
 MODULE_LICENSE("Dual BSD/GPL");
 
@@ -39,12 +102,13 @@ int log_execve(struct pt_regs *regs) {
 }
 
 static int __init execve_log_init(void) {
+    kln_p kln = get_kln_p();
 
     printk(KERN_INFO SOCKETLOG "proxyexec module has been loaded\n");
 
-    sys_call_table_addr = 0xffffffffc03b7388;
+    sys_call_table_addr = kln("sys_call_table");
 
-    printk(KERN_INFO SOCKETLOG "sys_call_table@%lx\n", sys_call_table_addr);
+    printk(KERN_INFO SOCKETLOG "sys_call_table %lx\n", sys_call_table_addr);
 
     enable_page_rw((void *)sys_call_table_addr);
     original_execve = ((syscall_wrapper *)sys_call_table_addr)[__NR_execve];
